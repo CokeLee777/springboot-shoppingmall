290303990f1613f497bd5d1b31d11bc581c43f93
CokeLee777 <akak640@kyonggi.ac.kr>
2021-12-22 22:21:58 +0900

    회원, 상품 레퍼지토리 생성
    회원, 상품 레퍼지토리를 추가하였습니다. #3

---
+++ src/main/java/com/shoppingmall/repository/ItemRepository.java
@@ -0,0 +1,11 @@
+package com.shoppingmall.repository;
+
+import com.shoppingmall.domain.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+
+}

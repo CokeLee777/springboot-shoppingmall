9929e078498d924e68d5c8c5c044257223a72990
CokeLee777 <akak640@kyonggi.ac.kr>
2022-01-12 14:28:04 +0900

    암호화 파일 생성

--- src/main/java/com/shoppingmall/CokeApplication.java
+++ src/main/java/com/shoppingmall/CokeApplication.java
@@ -9,14 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class CokeApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location=" +
-			"classpath:application.yml," +
-			"classpath:aws.yml";
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(CokeApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-						.run(args);
+		SpringApplication.run(CokeApplication.class, args);
 	}
 
 }
